59c9c50ac5c26c7f39be61e23b8c8eab66c42fb3
Sterden <53517039+nattakittl57@users.noreply.github.com>
2026-02-09 15:10:28 +0700

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import express, { Request, Response } from 'express';
-
-const app = express();
-app.use(express.json());
+import subjectsRouter from "./routes/subjects";
+import cors from "cors";
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 8000;
+
+const app = express();
+
+if(!process.env.FRONTEND_URL) {
+    throw new Error("Missing FRONTEND_URL");
+}
+
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    // allowedHeaders: ['Content-Type', 'Authorization'],
+}))
+app.use(express.json());
+
+app.use('/api/subjects', subjectsRouter);
+
+
 
 app.get('/', (_req: Request, res: Response) => {
     res.json({ message: 'Express + TypeScript server running' });
